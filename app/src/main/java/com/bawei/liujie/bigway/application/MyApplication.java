25571645393d671b6f9d925b78e918dd0289f7df
lichaohuilala <578078578@qq.com>
2017-08-10 11:22:18 +0800

    测试core 工具

---
+++ app/src/main/java/com/bawei/liujie/bigway/application/MyApplication.java
@@ -0,0 +1,15 @@
+package com.bawei.liujie.bigway.application;
+
+import android.app.Application;
+
+/**
+ * Created by lichaohui on 2017/8/10.
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+    //
+}
